0753b35eb46ffac613e847ba1049b207da684b53
zhicong.xian <zhicong.xian@hotmail.de>
2026-03-04 15:19:22 +0100

    slurm_config

--- slurm_config_pro_dsc_knn.sh
+++ slurm_config_pro_dsc_knn.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #
-#SBATCH --job-name=pro_cifar100
-#SBATCH --output=pro_cifar_100.txt
+#SBATCH --job-name=MCRcifa10
+#SBATCH --output=cifa10.txt
 #SBATCH --ntasks=1
-#SBATCH --time=5-23:00:00
+#SBATCH --time=10-00:00:00
 #SBATCH --gres=gpu:1
 
 # debug info
@@ -19,4 +19,4 @@ export BLAS=/usr/lib/x86_64-linux-gnu/blas/libblas.so.3
 export LAPACK=/usr/lib/x86_64-linux-gnu/lapack/liblapack.a
 # pip install -U pip setuptools wheel
 # train
-python3 ./main_knn_enhanced_cont.py --data=cifar10-mcr --epo=5000 >> pro_cifar_100_out.txt
+python3 ./main_knn_enhanced_cont.py --data=cifar10-mcr --epo=5000 >> cifa10_out.txt
