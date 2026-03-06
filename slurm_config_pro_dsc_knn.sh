69da65914a1ede9a3be242a9df1596c9b0ad010c
zhicong.xian <zhicong.xian@hotmail.de>
2026-03-06 03:23:54 +0100

    add files to gitignore

--- slurm_config_pro_dsc_knn.sh
+++ slurm_config_pro_dsc_knn.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
-#SBATCH --job-name=MCRcifa10
-#SBATCH --output=cifa10.txt
+#SBATCH --job-name=KnnCoil100
+#SBATCH --output=coil100_pretrain_knn.txt
 #SBATCH --ntasks=1
 #SBATCH --time=10-00:00:00
 #SBATCH --gres=gpu:1
@@ -19,4 +19,4 @@ export BLAS=/usr/lib/x86_64-linux-gnu/blas/libblas.so.3
 export LAPACK=/usr/lib/x86_64-linux-gnu/lapack/liblapack.a
 # pip install -U pip setuptools wheel
 # train
-python3 ./main_knn_enhanced_cont.py --data=cifar10-mcr --epo=5000 >> cifa10_out.txt
+python3 ./main_knn_enhanced_cont.py --data=coil100 --bs=120 --load_pretrain  >> coil100_pretrain_knn_out.txt
