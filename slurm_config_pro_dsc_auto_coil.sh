51240611207d6bbe21694a31d039943af97215ea
zhicong.xian <zhicong.xian@hotmail.de>
2026-05-11 00:29:30 +0200

    remove load pretrain

--- slurm_config_pro_dsc_auto_coil.sh
+++ slurm_config_pro_dsc_auto_coil.sh
@@ -19,5 +19,5 @@ export BLAS=/usr/lib/x86_64-linux-gnu/blas/libblas.so.3
 export LAPACK=/usr/lib/x86_64-linux-gnu/lapack/liblapack.a
 # pip install -U pip setuptools wheel
 # train
-python3 ./main_subspace_auto_gamma_l1_rerun.py --data=coil100 --experiment_name=coil100_auto_gamma_seed42_with_l1norm --seeds=[42] --load_pretrain  >> coil100_pretrain_knn_out.txt
+python3 ./main_subspace_auto_gamma_l1_rerun.py --data=coil100 --experiment_name=coil100_auto_gamma_seed42_with_l1norm --seeds=[42]   >> coil100_pretrain_knn_out.txt
 
