20b9f4aa37964c4719e202a43c42a0f98ebdec7e
zhicong.xian <zhicong.xian@hotmail.de>
2026-05-11 23:16:04 +0200

    save data

---
+++ slurm_config_pro_dsc_default_cifar10_mcr.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+#SBATCH --job-name=DefaultCifar10_mcr
+#SBATCH --output=cifar10_mcr_default.txt
+#SBATCH --ntasks=1
+#SBATCH --time=10-00:00:00
+#SBATCH --gres=gpu:1
+
+# debug info
+hostname
+which python3
+nvidia-smi
+
+env
+
+# venv
+source /home/wiss/xian/venvs/subspace_clustering_3_12/bin/activate
+export BLAS=/usr/lib/x86_64-linux-gnu/blas/libblas.so.3
+export LAPACK=/usr/lib/x86_64-linux-gnu/lapack/liblapack.a
+# pip install -U pip setuptools wheel
+# train
+python3 ./main_default_gamma.py --data=cifar10-mcr --experiment_name=cifar10_mcr_default_rerun >> cifar10_mcr_default_out.txt
+
