8ef25ef22ae46816a023f14ccf5809466f778814
zhicong.xian <zhicong.xian@hotmail.de>
2026-05-11 12:37:45 +0200

    save edit for more data

--- main_subspace_auto_gamma.py
+++ main_subspace_auto_gamma.py
@@ -400,7 +400,7 @@ for seed in args.seeds:
                                 l2_norm_b = np.linalg.norm(B, 2)
                                 soft_rank_global = frobi**2/(l2_norm_b**2 + 1e-16)
                                 # print("soft_rank_global", soft_rank_global)
-                                gamma_estimated = args.beta *  soft_rank_global / 4  # 1 / lambda_hat
+                                gamma_estimated = args.beta *  soft_rank_global   # 1 / lambda_hat
                             except Exception as e:
                                 print("trying to calculate soft rank ")
                                 print(e)
@@ -409,7 +409,7 @@ for seed in args.seeds:
                                     l2_norm_b = np.linalg.norm(B + 1e-16 * np.eye(len(B)), 2)
                                     soft_rank_global = frobi ** 2 / (l2_norm_b ** 2 + 1e-16)
                                     # print("soft_rank_global", soft_rank_global)
-                                    gamma_estimated = args.beta * soft_rank_global / 4
+                                    gamma_estimated = args.beta * soft_rank_global
                                 except Exception as e:
                                     print(e)
                                 gamma_estimated = gamma_previous
