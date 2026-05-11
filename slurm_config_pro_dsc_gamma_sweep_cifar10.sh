97cd52b88ce6c19232adf162c31fbf9b5b0950e7
zhicong.xian <zhicong.xian@hotmail.de>
2026-05-11 23:41:49 +0200

    cifar10

---
+++ slurm_config_pro_dsc_gamma_sweep_cifar10.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+#SBATCH --job-name=C10GamSwe
+#SBATCH --output=cifa10_gamma_sweep.txt
+#SBATCH --ntasks=1
+#SBATCH --time=10-00:00:00
+#SBATCH --gres=gpu:1
+
+# debug info
+hostname
+which python3
+nvidia-smi
+
+env
+
+# venv
+source /home/wiss/xian/venvs/subspace_clustering_3_12/bin/activate
+export BLAS=/usr/lib/x86_64-linux-gnu/blas/libblas.so.3
+export LAPACK=/usr/lib/x86_64-linux-gnu/lapack/liblapack.a
+# pip install -U pip setuptools wheel
+# train
+python3 ./main_gamma_sweep.py --data=cifar10 --experiment_name=wandb_sweep_cifar10 --seed=1 --epo=5000 >> cifa10_gamma_sweep_out.txt
+
