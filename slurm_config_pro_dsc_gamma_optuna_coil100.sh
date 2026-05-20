4bab0525921d23f71a47aaf1712a5a1b41a209fb
zhicong.xian <zhicong.xian@hotmail.de>
2026-05-20 18:09:08 +0200

    save data

--- slurm_config_pro_dsc_gamma_optuna_coil100.sh
+++ slurm_config_pro_dsc_gamma_optuna_coil100.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-#SBATCH --job-name=GamSwCoil100Rerun
+#SBATCH --job-name=OpCoil100
 #SBATCH --output=rerun_gamma_sweep_coil100.txt
 #SBATCH --ntasks=1
 #SBATCH --time=10-00:00:00
@@ -19,5 +19,5 @@ export BLAS=/usr/lib/x86_64-linux-gnu/blas/libblas.so.3
 export LAPACK=/usr/lib/x86_64-linux-gnu/lapack/liblapack.a
 # pip install -U pip setuptools wheel
 # train
-python3 ./main_subspace_gamma_optuna_automl.py --data=coil100 --experiment_name=coil100_optuna_automl --seeds=[0,1,2,3,4,5,,7,8,9] >> rerun_gamma_sweep_coil100_out.txt
+python3 ./main_subspace_gamma_optuna_automl.py --data=coil100 --experiment_name=coil100_optuna_automl >> rerun_gamma_sweep_coil100_out.txt
 
