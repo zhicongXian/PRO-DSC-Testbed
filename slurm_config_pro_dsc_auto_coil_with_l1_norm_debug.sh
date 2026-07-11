0e0a2d80fcc133bc180f79c9099c303afb70561c
zhicong.xian <zhicong.xian@hotmail.de>
2026-07-11 04:25:01 +0200

    resubmit

--- slurm_config_pro_dsc_auto_coil_with_l1_norm_debug.sh
+++ slurm_config_pro_dsc_auto_coil_with_l1_norm_debug.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-#SBATCH --job-name=DebugL1NormAutoCoil100
+#SBATCH --job-name=pro_dsc_DebugL1NormAutoCoil100
 #SBATCH --output=l1_norm_coil100_pretrain_knn_debug.txt
 #SBATCH --ntasks=1
 #SBATCH --time=10-00:00:00
@@ -19,5 +19,5 @@ export BLAS=/usr/lib/x86_64-linux-gnu/blas/libblas.so.3
 export LAPACK=/usr/lib/x86_64-linux-gnu/lapack/liblapack.a
 # pip install -U pip setuptools wheel
 # train
-python3 ./main_subspace_auto_gamma_with_l1_norm.py --data=coil100  --experiment_name=coil100_auto_gamma_with_si --load_pretrain --seeds=[42,0,1,2,3,4,5,6,7,8,9]   >> l1_norm_coil100_pretrain_knn_debug_out.txt
+python3 ./main_subspace_auto_gamma_with_l1_norm.py --data=coil100  --experiment_name=coil100_auto_gamma_with_si2 --load_pretrain --seeds=[42,0,1,2,3,4,5,6,7,8,9]   >> l1_norm_coil100_pretrain_knn_debug_out.txt
 
