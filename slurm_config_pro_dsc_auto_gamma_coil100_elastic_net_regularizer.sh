ecfe675407adc11ae69a60ea2dac77b915d92f70
zhicong.xian <zhicong.xian@hotmail.de>
2026-09-06 14:54:38 +0200

    try to resolve invalid json list error

--- slurm_config_pro_dsc_auto_gamma_coil100_elastic_net_regularizer.sh
+++ slurm_config_pro_dsc_auto_gamma_coil100_elastic_net_regularizer.sh
@@ -21,5 +21,5 @@ export LAPACK=/usr/lib/x86_64-linux-gnu/lapack/liblapack.a
 # train
 
 
-python3 ./main_subspace_auto_gamma_elastic_net_hpo.py --data=coil100 --seeds=[0,42, 1,2,3,4,5,6,7,8,9] --experiment_name=auto_gamma_coil100_elastic_net3
+python3 ./main_subspace_auto_gamma_elastic_net_hpo.py --data=coil100 --seeds=[0,42,1,2,3,4,5,6,7,8,9] --experiment_name=auto_gamma_coil100_elastic_net3
 
